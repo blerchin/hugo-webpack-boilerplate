a3e08de85a9844250e6caa9c8a9e9d351065fbbe
Ben Lerchin <ben@lerchin.com>
2017-06-28 16:47:22 -0700

    test scss compilation by adding red class in scss and js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 
 const welcome = document.createElement('h1');
 welcome.innerHTML = 'Welcome to Hugo Webpack <i class="fa fa-check-circle"></i>';
+welcome.classList.add('color--red');
 document.body.querySelector('.content').prepend(welcome);
 
 if (module.hot) {
